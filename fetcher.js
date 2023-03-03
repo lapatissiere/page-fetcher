a87cf62618fe3723ca4ec43e0c2af0f7576edb50
Nazia Farooq <lapatissiere@yahoo.com>
2023-03-03 12:23:06 -0500

    edited code

--- fetcher.js
+++ fetcher.js
@@ -1,34 +1,38 @@
 var http = require("http");
 const fs = require('fs');
 
+const args = process.argv;
+let url = args[2];
+let filepath = args[3];
+
 http.createServer(function (request, response) {
   // Send the HTTP header 
   // HTTP Status: 200 : OK
   // Content Type: text/plain
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('http://www.example.edu/\n');
+  response.end(`${url}\n`);
 }).listen(8081);
 
 const content = 'Some content!';
 
-fs.readFile('./index.html', 'utf8', (err, data) => {
+fs.readFile(`${filepath}`, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
+  //console.log(data);
 });
 
-fs.writeFile('./index.html', content, err => {
+fs.writeFile(`${filepath}`, content, err => {
   if (err) {
     console.error(err);
   }
   // file written successfully
 });
 
-const stats = fs.statSync('./index.html');
+const stats = fs.statSync(`${filepath}`);
 
-fs.stat('./index.html', (err, stats) => {
+fs.stat(`${filepath}`, (err, stats) => {
   if (err) {
     console.error(err);
     return;
@@ -37,4 +41,4 @@ fs.stat('./index.html', (err, stats) => {
 });
 
 // Console will print the message
-console.log(`Downloaded and saved ${stats.size} bytes to ./index.html`);
+console.log(`Downloaded and saved ${stats.size} bytes to ${filepath}`);
